bf73d709d89fd52f771fcea036c8a7f0c6b673c0
loeshat <90491848+loeshat@users.noreply.github.com>
2024-04-18 11:27:57 +1000

    updates 4/18

--- src/pages/WorkPage.jsx
+++ src/pages/WorkPage.jsx
@@ -6,6 +6,17 @@ import { ThemeProvider } from "@mui/material/styles";
 import { Container, Typography, Box, Divider } from "@mui/material";
 import TopNav from '../components/TopNav';
 import DownloadIcon from '@mui/icons-material/Download';
+import LanguageIcon from '@mui/icons-material/Language';
+
+
+const designProjectsInfo = [
+  {
+    title: "TangyTimeTable",
+    description: "A timetable scheduling assistant to plan group & friend catchups",
+    logo: "../leo-io/assets/tangy.png",
+    link: "https://www.figma.com/file/I7GMziARoK8euZ8TZevWRK/TangyTimeTable---HiFi-Prototype---Week-7-Deliverable---COMP4511?type=design&node-id=0%3A1&mode=design&t=eOq1y05zWXRlDX9x-1",
+  },
+]
 
 const caseCompetitionInfo = [
   {
@@ -38,8 +49,16 @@ const researchReports = [
   {
     title: "Artificial Intelligence in Accounting",
     download: "../leo-io/pdf/ai.pdf",
+  },
+  {
+    title: "AASB’s Exposure Draft for Sustainability - Impact on Australian Businesses",
+    download: "../leo-io/pdf/aasb.pdf"
   }
 ]
+
+const openLinkInNewTab = (url) => {
+  window.open(url, '_blank');
+};
 
 const HomePage = () => {
   const handleSlidesDownload = (href) => {
@@ -60,6 +79,29 @@ const HomePage = () => {
         <Container
           style={{ marginLeft: "3vw", marginTop: "10vh", marginBottom: "15vh" }}
         >
+          <Box>
+            <Typography variant="h3">design projects</Typography>
+            <Divider style={{ marginBottom: "30px", marginTop: "15px" }} />
+            <Typography variant="h5" style={{ marginBottom: "30px" }}>I'm a naturally creative person, so coming up with unique and good-looking designs are enjoyable for me. However, I've also begun applying design and heuristic principles towards my work</Typography>
+            <Box style={{ marginBottom: "50px" }}>
+              {designProjectsInfo.map((item, index) => (
+                <Box className="design-box">
+                  <Box component="img" src={item.logo} style={{ height: "150px", borderRadius: "15px" }} />
+                  <Box style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+                    <Box>
+                      <Typography variant="h6">{item.title}</Typography>
+                      <Typography variant="h5">{item.description}</Typography>
+                    </Box>
+                    <Box className="download-button" onClick={() => { openLinkInNewTab(item.link) }}>
+                      <LanguageIcon variant="lg" />
+                      <Typography variant="lg">View Figma</Typography>
+                    </Box>
+                  </Box>
+                </Box>
+              ))}
+            </Box>
+          </Box>
+
           <Typography variant="h3">case competitions and presentations</Typography>
           <Divider style={{ marginBottom: "30px", marginTop: "15px" }} />
           <Typography variant="h5" style={{ marginBottom: "30px" }}>Case competitions are a place where I can show off my teamwork and critical thinking skills to solve a problem. With a team, I must develop a strategy to solve the issue, then present the findings to industry individuals.</Typography>
@@ -84,15 +126,13 @@ const HomePage = () => {
             <Typography variant="h3">written reports</Typography>
             <Divider style={{ marginBottom: "30px", marginTop: "15px" }} />
             <Typography variant="h5" style={{ marginBottom: "30px" }}>Here are some personal research projects which I've been curious about throughout my young adult life. Report writing is an essential skill that is used throughout the workforce.</Typography>
-            <Box style={{ display: "flex", flexDirection: "row" }}>
+            <Box style={{ display: "flex", flexDirection: "row", gap: "15px" }}>
               {researchReports.map((item, index) => (
                 <Box className="report-box" onClick={() => handleSlidesDownload(item.download)}>
                   <Typography>{item.title}</Typography>
                 </Box>
               ))}
-
             </Box>
-
           </Box>
         </Container>
       </ThemeProvider>
